85583908107c81adc385cdcbf21143225168bf18
Jadasamantha <41348910+Jadasamantha@users.noreply.github.com>
2018-12-10 20:14:09 -0500

    Add files via upload

---
+++ online library/inc/data.php
@@ -0,0 +1,177 @@
+<?php
+$catalog = [];
+//Books
+$catalog[101] = [
+	"title" => "Decoded",
+	"img" => "img/media/decoded.jpg",
+    "genre" => "Non-fiction",
+    "format" => "Hardcover",
+    "year" => 2010,
+    "category" => "Books",
+    "authors" => [
+        "Jay Z"
+			],
+    "publisher" => "Spiegel & Grau",
+    "isbn" => '978-0812981155'
+];
+$catalog[102] = [
+    "title" => "The Heart Of A Woman",
+    "img" => "img/media/heart.jpg",
+    "genre" => "Autobiography",
+    "format" => "Paperback",
+    "year" => 1981,
+    "category" => "Books",
+    "authors" => [
+        "Maya Angelou"
+    ],
+    "publisher" => "Penguin Random House LLC.",
+    "isbn" => '978-0812980325'
+];
+$catalog[103] = [
+    "title" => "The Coldest Winter Ever",
+    "img" => "img/media/coldestwinter.jpg",
+    "genre" => "Urban fiction",
+    "format" => "Paperback",
+    "year" => 1999,
+    "category" => "Books",
+    "authors" => [
+        "Sister Souljah"
+    ],
+    "publisher" => "Pocket Star",
+    "isbn" => '978-1416521693'
+];
+$catalog[104] = [
+    "title" => "Around the Way Girl: A Memoir",
+    "img" => "img/media/aroundtheway.jpg",
+    "genre" => "Autobiography",
+    "format" => "Hardcover",
+    "year" => 2016,
+    "category" => "Books",
+    "authors" => [
+        "Taraji P. Henson"
+    ],
+    "publisher" => "Atria / 37 INK",
+    "isbn" => '978-1501126000'
+];
+//Movies
+$catalog[201] = [
+    "title" => "For Colored Girls",
+    "img" => "img/media/coloredgirls.jpg",
+    "genre" => "Drama",
+    "format" => "DVD",
+    "year" => 1994,
+    "category" => "Movies",
+    "director" => "Robert Zemeckis",
+    "writers" => [
+        "Ntozake Shange",
+        "Tyler Perry"
+    ],
+    "stars" => [
+        "Tom Hanks",
+        "Rebecca Williams",
+        "Sally Field",
+        "Michael Conner Humphreys"
+    ]
+];
+$catalog[202] = [
+    "title" => "Set It Off",
+    "img" => "img/media/setitoff.jpg",
+    "genre" => "Drama/ Thriller",
+    "format" => "New Line Cinema",
+    "year" => 1996,
+    "category" => "Movies",
+    "director" => "Gary Gray",
+    "writers" => [
+        "Tukashi Bufford",
+				"Kate Lanier"
+    ],
+    "stars" => [
+        "Jada Pinkett",
+        "Queen Latifah",
+        "Vivica A. Fox",
+        "Kimberly Elise",
+        "John C. McGinley",
+    ]
+];
+$catalog[203] = [
+    "title" => "Freedom Writers",
+    "img" => "img/media/freedomwriters.jpg",
+    "genre" => "Drama/ Teen Film",
+    "format" => "Paramount Pictures",
+    "year" => 2007,
+    "category" => "Movies",
+    "director" => "Richard LaGravenese",
+    "writers" => [
+        "Richard LaGravenese"
+    ],
+    "stars" => [
+        "Hilary Swank",
+        "Scott Glenn",
+        "Imelda Staunton",
+        "Patrick Dempsey",
+        "Mario",
+    ]
+];
+$catalog[204] = [
+    "title" => "Paid In Full",
+    "img" => "img/media/paidinfull.jpg",
+    "genre" => "Comedy",
+    "format" => "DVD",
+    "year" => 2002,
+    "category" => "Movies",
+    "director" => "Rob Reiner",
+    "writers" => [
+        "Matthew Cirulnick",
+				"Thulani Davis"
+    ],
+    "stars" => [
+        "Wood Harris",
+        "Regina Hall",
+        "Kevin Carroll",
+        "Esai Morales",
+        "Cynthia Martells",
+        "Remo Green",
+        "Ron Cephas Jones",
+        "Cam'Ron",
+        "Chi McBride",
+        "Mekhi Phifer"
+    ]
+];
+//Music
+$catalog[301] = [
+    "title" => "Heart Break Kodak",
+    "img" => "img/media/heartbreak.jpeg",
+    "genre" => "Hip Hop",
+    "format" => "CD",
+    "year" => 2012,
+    "category" => "Music",
+    "artist" => "Kodak Black"
+];
+$catalog[302] = [
+    "title" => "Wins and Losses",
+    "img" => "img/media/wandl.jpg",
+    "genre" => "Rap",
+    "format" => "Studio Album",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Meek Mill"
+];
+$catalog[303] = [
+    "title" => "Super Slimey",
+    "img" => "img/media/superslimey.jpeg",
+    "genre" => "Hip Hop",
+    "format" => "Mixtape",
+    "year" => 2017,
+    "category" => "Music",
+    "artist" => "Future",
+		           "Young Thug"
+];
+$catalog[304] = [
+    "title" => "Memories Don't Die",
+    "img" => "img/media/memdd.jpg",
+    "genre" => "R&B",
+    "format" => "Studio Album",
+    "year" => 2018,
+    "category" => "Music",
+    "artist" => "Tory Lanez"
+];
